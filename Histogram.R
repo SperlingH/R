1c3053cd913f592b48ea30c50dd3178bbb8334c4
SperlingH <hts76@web.de>
2015-10-28 10:40:42 +0100

    clearer metadata

--- Histogram.R
+++ Histogram.R
@@ -19,11 +19,12 @@ ggplot(CO2, aes(x = uptake)) +
 		colour = "black", 
 		fill = "grey")
 
-
+## Plot histogram with function overlay
 x <- rnorm(100, mean=5, sd=0.0001)
 df <- data.frame(counts=x, cat=1)
 
 ggplot(df, aes(x=counts)) +
-	labs(y="counts", x="values")+ 
+	labs(y="counts", x="values") + 
 	geom_histogram(aes(y = ..density..), color="black", fill=NA) +
-	stat_function(fun = dnorm, col = "blue",arg=list(mean=5,sd=0.0001))+ theme(axis.ticks = element_blank(), axis.text = element_blank())
+	stat_function(fun = dnorm, col = "blue",arg=list(mean=5,sd=0.0001)) + 
+  theme(axis.ticks = element_blank(), axis.text = element_blank())
