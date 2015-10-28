8ab0d223954026179d5a7590866d2863d9399dec
SperlingH <hts76@web.de>
2015-10-28 11:02:44 +0100

    clearer metadata & example

--- get.combinations.R
+++ get.combinations.R
@@ -1,16 +1,24 @@
-# Creation date: 2015-09-28
-# Created by: HS
-# Objective: Get all combinations of a m*m matrix without diagonals and reverse
-# Usage: get.combinations(n) # n is an integer with the rows and columns of a n*n matrix
-# Input: n - integer
-# Output: 
-# Misc: Needs further work to return the integers for further use in other functions
+# Creation date: 
+#   2015-09-28
+# Created by: 
+#   HS
+# Objective: 
+#   Get all combinations of a m*m matrix without diagonals and reverse
+# Usage: 
+#   get.combinations(n) # n is an integer with the rows and columns of a n*n matrix
+# Input: 
+#   n - integer
+# Output:
+#   
+# Misc: 
+#   Needs further work to return the integers for further use in other functions
 
 ## pairwise output of possible combination of "n" elements 
 get.combinations <- function(n) {
 for (i in 1:(n-1)){
   for (j in (i+1):n){
     print(paste(i,j)) # test of function - other functions can be implemented with the use of [i] and [j]
+    # print(paste(i, "*", j, "=", i*j))
   }
   #return ()
 }
